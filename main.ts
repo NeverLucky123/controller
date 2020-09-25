177229896eb5c153062c73ac6662ae5d5edd8f3f
nadineoliynyk <71521585+nadineoliynyk@users.noreply.github.com>
2020-09-25 10:19:17 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.A)
+    light.showAnimation(light.rainbowAnimation, 100)
 })
 input.buttonB.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.B)
+    light.showAnimation(light.cometAnimation, 100)
 })
