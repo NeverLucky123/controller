0f5e369171062593b0682bcf1e56ce0496109192
nadineoliynyk <71521585+nadineoliynyk@users.noreply.github.com>
2020-09-25 14:36:35 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 input.onLoudSound(function () {
     makerController.player1.press(ArcadeButton.Right)
-    light.setAll(0x00ff00)
-    music.playMelody("F G A B A G F G ", 120)
+    light.showAnimation(light.runningLightsAnimation, 500)
+    light.clear()
 })
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.A)
