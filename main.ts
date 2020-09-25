7457ef8104d1f4b35a50331e2fc8535d5404470b
Henry Zhou <43188301+NeverLucky123@users.noreply.github.com>
2020-09-25 11:50:39 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,15 +3,14 @@ input.onLoudSound(function () {
     light.showAnimation(light.runningLightsAnimation, 500)
     light.clear()
 })
+input.buttonB.onEvent(ButtonEvent.Down, function () {
+    makerController.player1.setButton(ArcadeButton.B, true)
+    light.showAnimation(light.cometAnimation, 200)
+    light.clear()
+})
 input.buttonA.onEvent(ButtonEvent.Click, function () {
     makerController.player1.press(ArcadeButton.A)
     light.showAnimation(light.rainbowAnimation, 200)
-    music.baDing.play()
     light.clear()
 })
-input.buttonB.onEvent(ButtonEvent.Click, function () {
-    makerController.player1.press(ArcadeButton.B)
-    light.showAnimation(light.cometAnimation, 200)
-    music.baDing.play()
-    light.clear()
-})
+input.setLoudSoundThreshold(255)
